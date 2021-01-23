ee47b952fcb57f33895d4622591e06c62eb00e8d
overnightJustifier <74424414+overnightJustifier@users.noreply.github.com>
2021-01-23 22:21:38 +0300

    Update Insertion_Sort.py

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,14 +1,13 @@
-def insertion_sort(InputList):
-    for i in range(1, len(InputList)):
+def insertion_sort(InputList, inputLength):
+    for i in range(1, inputLength):
         j = i-1
-        nxt_element = InputList[i]
+        next_element = InputList[i]
 # Compare the current element with next one
-		
-        while (InputList[j] > nxt_element) and (j >= 0):
+        while (InputList[j] > next_element) and (j >= 0):
             InputList[j+1] = InputList[j]
             j=j-1
         InputList[j+1] = nxt_element
 # you can add list of any numbers
-list = [6,5,3,1,8,7,2,4]
+list = [6, 5, 3, 1, 8]
 insertion_sort(list)
 print(list)
